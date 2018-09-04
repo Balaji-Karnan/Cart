833ca93b3e8a81f634a4eaed4ee74c4e095df3bb
Balaji-Karnan <balajirocky321@gmail.com>
2018-09-04 08:53:52 +0530

    first commit

---
+++ src/components/Front.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './Front.css';
+import './Form.css';
+
+class Front extends Component{
+    render(){
+        return(
+            <div className="bg" style={ { backgroundImage: 'url("images/download.webp")' } }>
+            </div>
+        )
+    }
+}
+
+export default Front;
---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'SAMSUNG',
+    price: 13.999,
+    currency: 'Rs',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'APPLE',
+    price: 99.599,
+    currency: 'Rs',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'HONOR',
+    price: 9.999,
+    currency: 'Rs',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'NOKIA',
+    price: 15.499,
+    currency: 'Rs',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'MOTO',
+    price: 12.999,
+    currency: 'Rs',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'RedMi',
+    price: 14.999,
+    currency: 'Rs',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
